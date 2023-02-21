91258879e7cdbf62f962424adee2d72f9ec05dc8
gcm <gcuatem@yahoo.com.mx>
2023-02-21 11:13:11 -0600

    c2

---
+++ Shared/BlobContainerNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+
+  public static class BlobContainerNames
+  {
+    public const string ImageContainer = "postscontainer";
+    public const string MarkdownContainer = "postscontainer";
+  }
+}
